270d6f5a497c9c0fc2d14c1cacb1ff68953d31d6
Maxmiliano <maiadefreitas@hotmail.com>
2023-08-09 22:23:07 -0300

    Deixar responsivo para celular

--- y/js/register.js
+++ y/js/register.js
@@ -1,3 +1,11 @@
+function logout(){
+    firebase.auth().signOut().then(()=>{
+        window.location.href = "../../index.html";
+    }).catch(()=>{
+        alert('Erro ao fazer logout');
+    })  
+}
+
 firebase.auth().onAuthStateChanged(function(user){
     if(user){
         window.location = "../home/home.html";
